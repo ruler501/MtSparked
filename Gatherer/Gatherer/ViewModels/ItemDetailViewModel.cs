54fcb91067bad84f8988edb7ac97941958f8bdce
Devon Richards <friends.devon@gmail.com>
2018-02-15 19:02:40 -0600

    Add project files.

---
+++ Gatherer/Gatherer/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Gatherer.Models;
+
+namespace Gatherer.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
