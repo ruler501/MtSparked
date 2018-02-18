68641ea9fc2ac3ac4bb1d9f443a6a8b512be931d
Devon Richards <friends.devon@gmail.com>
2018-02-17 21:27:39 -0600

    Add Card Details page

--- Gatherer/Gatherer/Views/CardPage.xaml.cs
+++ Gatherer/Gatherer/Views/CardPage.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using Gatherer.Models;
-using Gatherer.Services;
-using Gatherer.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,40 +13,13 @@ namespace Gatherer.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CardPage : ContentPage
 	{
-        CardsViewModel viewModel;
+        Card Card;
 
-        public CardPage()
-        {
-            InitializeComponent();
+		public CardPage (Card card)
+		{
+			InitializeComponent ();
 
-            var query = CardDataStore.Where("TypeLine", "Contains", "Planeswalker").Find();
-
-            BindingContext = viewModel = new CardsViewModel(query);
+            this.BindingContext = Card = card;
         }
-
-        public CardPage(CardDataStore cards)
-        {
-            InitializeComponent();
-            
-            BindingContext = viewModel = new CardsViewModel(cards);
-        }
-
-        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
-        {
-            // Manually deselect item.
-            CardsListView.SelectedItem = null;
-        }
-
-        async void AddItem_Clicked(object sender, EventArgs e)
-        {
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-
-            if (viewModel.Items.Count == 0)
-                viewModel.LoadItemsCommand.Execute(null);
-        }
-    }
+	}
 }
---
+++ Gatherer/Gatherer/Views/CardsListPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Gatherer.Models;
+using Gatherer.Services;
+using Gatherer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gatherer.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CardsListPage : ContentPage
+	{
+        CardsViewModel viewModel;
+
+        public CardsListPage()
+        {
+            InitializeComponent();
+
+            var query = CardDataStore.Where("TypeLine", "Contains", "Planeswalker").Find();
+
+            BindingContext = viewModel = new CardsViewModel(query);
+        }
+
+        public CardsListPage(CardDataStore cards)
+        {
+            InitializeComponent();
+            
+            BindingContext = viewModel = new CardsViewModel(cards);
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            if(args.SelectedItem is null)
+            {
+                return;
+            }
+            Card card = (Card)args.SelectedItem;
+            Navigation.PushAsync(new CardPage(card));
+            // Manually deselect item.
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
