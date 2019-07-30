5eb1df012b71f2c34ac268ed79decf4ea234a278
Devon Richards <friends.devon@gmail.com>
2019-07-30 12:28:24 -0500

    Fix some of hierarchies of IFormat

--- MtSparked/MtSparked.Interop/Services/Formatting/FormatBase.cs
+++ MtSparked/MtSparked.Interop/Services/Formatting/FormatBase.cs
@@ -2,9 +2,9 @@
 using MtSparked.Interop.Utils;
 
 namespace MtSparked.Interop.Services.Formatting {
-    public abstract class IFormatBase : IFormat {
+    public abstract class FormatBase : IFormat {
 
-        protected IFormatBase(string name, string description, IDictionary<string, object> formatOptions, string version) {
+        protected FormatBase(string name, string description, IDictionary<string, object> formatOptions, string version) {
             this.Name = name;
             this.Description = description;
             this.FormatOptions = formatOptions;
@@ -18,7 +18,7 @@ namespace MtSparked.Interop.Services.Formatting {
 
     }
 
-    public class ReversedFormat<Format1, T, FormatResult> : IFormatBase, IFormat<FormatResult, T>
+    public class ReversedFormat<Format1, T, FormatResult> : FormatBase, IFormat<FormatResult, T>
             where Format1 : FormatBase<T, FormatResult> {
 
         internal Format1 ReversedInstance { get; }
@@ -51,66 +51,59 @@ namespace MtSparked.Interop.Services.Formatting {
                 : base(name, description, formatOptions, version) { }
     }
 
-    public sealed class AnyFormat<T> : FormatBase<Formatted<T>.Any, T> {
+    public sealed class AnyFormat<T> : FormatBase, IOutFormat<T, Formatted<T>.Any> {
 
         public AnyFormat()
-                : base("Any<" + typeof(T).Name + "> to " + typeof(T).Name, "Add or remove Any qualification",
+                : base(typeof(T).Name + " to Any<" + typeof(T).Name + ">", "Add Any qualification",
                        new Dictionary<string, object>(), "v1.0") { }
 
         // Implicit conversions on Any make this work.
-        public override T Format(Formatted<T>.Any model) => model;
-        public override Formatted<T>.Any Parse(T formattedModel) => formattedModel;
+        public Formatted<T>.Any Format(T model) => model;
+    }
+
+    public sealed class AnyFromMarkedFormat<T, Marker> : FormatBase, IOutFormat<Formatted<T>.For<Marker>, Formatted<T>.Any> {
+
+        public AnyFromMarkedFormat()
+                : base("For<" + typeof(T).Name + ", " + typeof(Marker).Name + "> to Any<" + typeof(T).Name + ">", "Add Any qualification",
+                       new Dictionary<string, object>(), "v1.0") { }
+
+        // Implicit conversions on Any and For make this work.
+        public Formatted<T>.Any Format(Formatted<T>.For<Marker> model) => model;
 
     }
 
-    public abstract class ChainFormatPre<Format1, T, Mid, FormattedMid, Format2, FormatResult, FormattedResult>
-              : FormatBase<T, Mid, FormattedMid>,
-                IFormat<FormattedMid, FormatResult, FormattedResult>
-            where FormattedMid : Formatted<Mid>
-            where FormattedResult : Formatted<FormatResult>
-            where Format1 : IFormat<T, Mid, FormattedMid>
-            where Format2 : IFormat<FormattedMid, FormatResult, FormattedResult> {
+    public class ChainFormatOut<T, Mid, FormatResult>
+            : FormatBase, IOutFormat<T, FormatResult> {
 
-        internal ChainFormatPre(Format1 format1, Format2 format2)
+        private IOutFormat<T, Mid> FormatInstance1 { get; }
+        private IOutFormat<Mid, FormatResult> FormatInstance2 { get; }
+
+        public ChainFormatOut(IOutFormat<T, Mid> format1, IOutFormat<Mid, FormatResult> format2)
                 : base(format1.Name + " / " + format2.Name, format1.Description + "\n/\n" + format2.Description,
                        GenericExtensions.CombineDicts(format1.FormatOptions, format2.FormatOptions),
                        format1.Version + "/" + format2.Version) {
-            this.Format1Instance = format1;
-            this.Format2Instance = format2;
+            this.FormatInstance1 = format1;
+            this.FormatInstance2 = format2;
         }
 
-        public Format1 Format1Instance { get; }
-        public Format2 Format2Instance { get; }
-
-        // It has the same argument as the version from ReversedFormat.
-        // Can still call the original by casting up to it.
-        FormattedResult IOutFormat<FormattedMid, FormattedResult>.Format(FormattedMid model) => this.Format2Instance.Format(model);
-        public FormattedMid Parse(FormattedResult formattedModel) => this.Format2Instance.Parse(formattedModel);
+        public FormatResult Format(T model) => this.FormatInstance2.Format(this.FormatInstance1.Format(model));
     }
 
-    // We have to split ChainFormatPre off because otherwise it will fail to compile since it would
-    // technically be possible that the parameters could be specified so that the IFormat interfaces
-    // would be the same which is not allowed. Moving them to separate classes makes it work though.
-    public sealed class ChainFormat<Format1, T, Mid, FormattedMid, Format2, FormatResult, FormattedResult>
-              : ChainFormatPre<Format1, T, Mid, FormattedMid, Format2, FormatResult, FormattedResult>,
-                IFormat<T, FormatResult, FormattedResult>
+
+    public class ChainFormatOut<T, Mid, FormattedMid, FormatResult, FormattedResult>
+            : ChainFormatOut<T, FormattedMid, FormattedResult>,
+              IOutFormat<T, FormatResult, FormattedResult>
             where FormattedMid : Formatted<Mid>
-            where FormattedResult : Formatted<FormatResult>
-            where Format1 : IFormat<T, Mid, FormattedMid>
-            where Format2 : IFormat<FormattedMid, FormatResult, FormattedResult> {
+            where FormattedResult : Formatted<FormatResult> {
 
-        public ChainFormat(Format1 format1, Format2 format2) : base(format1, format2) { }
+        public ChainFormatOut(IOutFormat<T, Mid, FormattedMid> format1, IOutFormat<FormattedMid, FormatResult, FormattedResult> format2)
+               : base(format1, format2) { }
 
-        public override FormattedMid Format(T model) => this.Format1Instance.Format(model);
-        public override T Parse(FormattedMid formattedModel) => this.Format1Instance.Parse(formattedModel);
-        FormattedResult IOutFormat<T, FormattedResult>.Format(T model) => this.Format2Instance.Format(this.Format1Instance.Format(model));
-        T IInFormat<T, FormattedResult>.Parse(FormattedResult formattedModel) => this.Format1Instance.Parse(this.Format2Instance.Parse(formattedModel));
 
         // Doesn't compile because ChainFormat is not one of the arguments.
         // Putting it on FormatBase doesn't work eitheer because it only has the type info for Format1(or Format2)
         // but you need both and you can't have generics on types. Maybe a subclass with implicit conversions could work?
         // public static ChainFormat<Format1, T, Mid, FormattedMid, Format2, FormatResult, FormattedResult> operator|(Format1 format1, Format2 format2)
         //    => new ChainFormat<Format1, T, Mid, FormattedMid, Format2, FormatResult, FormattedResult>(format1, format2);
-
     }
 }
--- MtSparked/MtSparked.Interop/Services/Formatting/IFormat.cs
+++ MtSparked/MtSparked.Interop/Services/Formatting/IFormat.cs
@@ -24,20 +24,32 @@ namespace MtSparked.Interop.Services.Formatting {
 
     }
 
+    public interface IOutFormat<T, FormatResult, FormattedResult>
+            : IOutFormat<T, FormattedResult>
+            where FormattedResult : Formatted<FormatResult> { }
+
     public interface IInFormat<out T, in FormatResult> : IFormat {
 
         T Parse(FormatResult formattedModel);
 
     }
 
+    public interface IInFormat<T, FormatResult, FormattedResult>
+            : IInFormat<T, FormattedResult>
+            where FormattedResult : Formatted<FormatResult> { }
+
     public interface IFormat<T, FormatResult>
             : IOutFormat<T, FormatResult>,
               IInFormat<T, FormatResult> { }
 
     public interface IFormat<T, FormatResult, FormattedResult>
-            : IFormat<T, FormattedResult>
+            : IFormat<T, FormattedResult>,
+              IOutFormat<T, FormatResult, FormattedResult>,
+              IInFormat<T, FormatResult, FormattedResult>
             where FormattedResult : Formatted<FormatResult> { }
 
+    // Helper classes so you don't have to reference Formatted directly.
+    // Should not be used for reflection.
     public interface IFormatToAny<T, FormatResult>
             : IFormat<T, FormatResult, Formatted<FormatResult>.Any> { }
 
